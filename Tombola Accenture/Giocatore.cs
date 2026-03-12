c7e45e1ed09a0f46237fbe654ad5096cf533c411
Michael Farinelli <michael.fari169@gmail.com>
2026-03-12 17:03:22 +0100

    classi Giocatore, Party, Partita

--- Tombola Accenture/Giocatore.cs
+++ Tombola Accenture/Giocatore.cs
@@ -2,5 +2,36 @@ namespace Tombola_Accenture;
 
 public class Giocatore
 {
+    public string nome;
+    public List<int> cartelle;
+    public double portafogli;
+
+    public void acquista_cartella(List<int> cartelle_disponibili, double costo_cartella)
+    {
+        Console.WriteLine("Scegli il numero della cartella");
+        Console.WriteLine("Questa è la lista delle cartelle disponibili");
+        int cartella = int.Parse(Console.ReadLine());
+        if (cartelle_disponibili.Contains(cartella))
+        {
+            
+            cartelle.Add(cartella);
+            portafogli -= costo_cartella;
+            Console.WriteLine($"{nome}, hai acquistato la cartella numero {cartella}");
+            Console.WriteLine($"Ora il tuo saldo è {portafogli}");
+            Console.WriteLine($"Le tue cartelle sono {cartelle}");
+        }
+        else
+        {
+            Console.WriteLine("La cartella che hai scelto non è disponibile");
+            Console.WriteLine("Scegli un'altra cartella");
+        }
+    }
+
+    public void incassa_premi(double premio)
+    {
+        Console.WriteLine($"complimenti {nome}, hai ottenuto una vincita di {premio} da una delle tue cartelle");
+        portafogli += premio;
+        Console.WriteLine($"Ora il tuo saldo è {portafogli}");
+    }
     
 }
--- Tombola Accenture/Party.cs
+++ Tombola Accenture/Party.cs
@@ -2,5 +2,40 @@ namespace Tombola_Accenture;
 
 public class Party
 {
-    
+    public string nome;
+    public List<string> giocatori;
+
+    public void aggiungi_giocatore()
+    {
+        Console.WriteLine("Scrivi il tuo nickname per unirti al Party");
+        string giocatore =  Console.ReadLine();
+        if (giocatori.Contains(giocatore))
+        {
+            Console.WriteLine("Il nickname scelto è già in uso in questo Party");
+            
+        }
+        else
+        {
+            giocatori.Add(giocatore);
+            Console.WriteLine($"complimenti {giocatore}, sei stato aggiunto al Party {nome}");
+            Console.WriteLine($"Ora il Party è composto dai giocatori {giocatori}");
+        }
+
+    }
+
+    public void rimuovi_giocatore()
+    {
+        Console.WriteLine("Scrivi il nickname del giocatore che vuoi rimuovere");  
+        string giocatore = Console.ReadLine();
+        if (giocatore.Contains(giocatore))
+        {
+            giocatori.Remove(giocatore);
+            Console.WriteLine($"Il giocatore {giocatore} è stato rimosso");
+            Console.WriteLine($"Ora il Party è composto dai giocatori {giocatori}");
+        }
+        else
+        {
+            Console.WriteLine($"il giocatore {giocatore} non è presente nel Party");
+        }
+    }
 }
